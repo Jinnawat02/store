79ced77a3db7a39ed55dd4277a57d81582f15c85
Jinnawat02 <tottnt765@gmail.com>
2026-05-12 11:27:23 +0700

    feat: Implement ProductsController with CRUD actions, views, and routes

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,48 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @products = Product.order(:id)
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_param)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_param)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_param
+    params.expect(product: [ :name ])
+  end
+end
