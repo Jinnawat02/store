4a1c53e69ded7c9ac41d119c36fd1a409953e346
Jinnawat02 <tottnt765@gmail.com>
2026-05-12 11:42:11 +0700

    feat: Add Category model, migrations, fixtures, and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_12_024800) do
-  create_table "products", force: :cascade do |t|
+ActiveRecord::Schema[8.1].define(version: 2026_05_12_043150) do
+  create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
   end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
