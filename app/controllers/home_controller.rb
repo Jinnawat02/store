324c022918ef723366b4d71a3bfe58d5eb2fd23d
Jinnawat02 <tottnt765@gmail.com>
2026-05-12 17:15:48 +0700

    feat: Update home#index to display recent products, categories, and shops with counts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @products_count = Product.count
-    @categories_count = Category.count
-    @shops_count = Shop.count
+    @recent_products = Product.order(created_at: :desc)
+    @recent_categories = Category.order(created_at: :desc)
+    @recent_shops = Shop.order(created_at: :desc)
 
-    @recent_products = Product.order(created_at: :desc).limit(5)
-    @recent_categories = Category.order(created_at: :desc).limit(5)
-    @recent_shops = Shop.order(created_at: :desc).limit(5)
+    @products_count = @recent_products.count
+    @categories_count = @recent_categories.count
+    @shops_count = @recent_shops.count
   end
 end
